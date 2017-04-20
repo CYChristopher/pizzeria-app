a17c5f57fb1a899d213cd6160402e5ef44f2f50e
David Le Hardy <lehardy.david@live.fr>
2017-04-20 16:19:56 +0200

    #4 Crud EJB dans pizzaService

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/PizzaService.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/metier/PizzaService.java
@@ -9,11 +9,35 @@ import javax.persistence.PersistenceContext;
 @Stateless
 public class PizzaService {
 
-  @PersistenceContext private EntityManager em;
+	@PersistenceContext
+	private EntityManager em;
 
+	public List<Pizza> findAll() {
+		return em.createQuery("select p from Pizza p", Pizza.class).getResultList();
+	}
 
-  public List<Pizza> findAll() {
-    return em.createQuery("select p from Pizza p", Pizza.class).getResultList();
-  }
+	public void update(String code, Pizza pizza) {
+
+		Pizza pizzaMod = em.createQuery("select piz from Pizza piz where piz.code=:codP", Pizza.class)
+				.setParameter("codP", code).getSingleResult();
+		pizza.setId(pizzaMod.getId());
+		em.merge(pizza);
+
+	}
+
+	public void delete(String code) {
+
+		Pizza pizzaDel = em.createQuery("select piz from Pizza piz where piz.code=:codP", Pizza.class)
+				.setParameter("codP", code).getSingleResult();
+
+		em.remove(pizzaDel);
+
+	}
+
+	public void save(Pizza pizza) {
+
+		em.persist(pizza);
+
+	}
 
 }
