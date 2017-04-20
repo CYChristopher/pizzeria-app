351c79fc76de2975c87a2f762b77bea4fad4ff73
Strauss72 <loicguen@gmail.com>
2017-04-20 17:35:13 +0200

    #17 usw002  beginning

--- pizzeria-website/src/index.js
+++ pizzeria-website/src/index.js
@@ -7,4 +7,6 @@ angular.module('pizzeria', [ngRoute, home])
     .config(function ($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $routeProvider.otherwise('/')
-    });
+    })
+
+.component('pizza',pizzaComponent)
