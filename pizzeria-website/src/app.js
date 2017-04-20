369a672801727847e1bd0c081172c365be98a4c4
Paola Lambroni <paolalambroni@gmail.com>
2017-04-20 17:33:50 +0200

    #16 usw001 accueil

--- pizzeria-website/src/app.js
+++ pizzeria-website/src/app.js
@@ -1,10 +1,19 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
-import home from './home'
+
+import { HomeComponent } from './home.component'
 
 
-angular.module('pizzeria', [ngRoute, home])
-    .config(function ($routeProvider, $locationProvider) {
+angular.module('pizzeria', [ngRoute])
+    .config(function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
-        $routeProvider.otherwise('/')
-    });
+        $routeProvider
+            .when('/', {
+                template: '<home></home>'
+            })
+            .otherwise('/')
+    })
+
+.component('home', HomeComponent)
+
+;
---
+++ pizzeria-website/src/home.component/index.js
@@ -0,0 +1,14 @@
+import template from "./home.html"
+import css from "./home.css"
+
+class controller {
+    constructor() {
+
+    }
+}
+
+export const HomeComponent = {
+    bindings: {},
+    template,
+    controller,
+}
