c4a9cf9bc43c3ecf4b8dcfd33f83e9ed9cb9a8cb
Paola Lambroni <paolalambroni@gmail.com>
2017-04-21 21:05:35 +0200

    #16 usw001 accueil

--- pizzeria-website/src/home.component/index.js
+++ pizzeria-website/src/home.component/index.js
@@ -4,6 +4,16 @@ import template from "./home.html"
 class controller {
     constructor(PizzaService) {
         this.PizzaService = PizzaService;
+        this.pizzas = this.PizzaService.getPizzas() ;
+        this.last3 = [] ;
+        this.getLast3() ;
+        this.test = "test";
+    }
+
+    getLast3() {
+        this.pizzas.then(pizza => this.last3.push(pizza[pizza.length-1])) ;
+        this.pizzas.then(pizza => this.last3.push(pizza[pizza.length-2])) ;
+        this.pizzas.then(pizza => this.last3.push(pizza[pizza.length-3])) ;
     }
 
 }
