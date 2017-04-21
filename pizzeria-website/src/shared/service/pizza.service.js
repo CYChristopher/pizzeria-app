a9b813c26474f7900587ec57aa9fb499208a381e
Paola Lambroni <paolalambroni@gmail.com>
2017-04-21 21:05:35 +0200

    #16 usw001 accueil

--- pizzeria-website/src/shared/service/pizza.service.js
+++ pizzeria-website/src/shared/service/pizza.service.js
@@ -1,1 +1,16 @@
-// TODO Service Pizza
+
+export class PizzaService {
+    constructor ($http) {
+        this.$http = $http ;
+        this.API_URL = "http://localhost:3000/api-pizzas";
+    }
+
+    getPizzas() {
+        return this.$http.get(this.API_URL)
+            .then(response => response.data);
+    }
+
+   
+
+
+}
