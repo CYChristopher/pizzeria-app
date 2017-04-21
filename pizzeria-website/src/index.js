6cf5bbe7eff6e4a6cf0890b668ebfb09e649bce2
Strauss72 <loicguen@gmail.com>
2017-04-21 12:59:57 +0200

    #17 usw002 continue

--- pizzeria-website/src/index.js
+++ pizzeria-website/src/index.js
@@ -2,11 +2,20 @@ import angular from 'angular'
 import ngRoute from 'angular-route'
 import home from './home'
 
+import {routes} from './routes'
+import {PizzaComponent} from './pizza.component'
+import {ListePizzasComponent} from './listePizzas.component'
+
+import {PizzaService} from './shared/service/pizza.service'
+
 
 angular.module('pizzeria', [ngRoute, home])
+	.config(routes)
     .config(function ($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
-        $routeProvider.otherwise('/')
     })
 
-.component('pizza',pizzaComponent)
+.component('pizza',PizzaComponent)
+.component('listePizzas',ListePizzasComponent)
+
+.service('PizzaService',PizzaService)
--- pizzeria-website/src/shared/service/pizza.service.js
+++ pizzeria-website/src/shared/service/pizza.service.js
@@ -1,1 +1,10 @@
-// TODO Service Pizza
+export class PizzaService{
+	
+	constructor($http){
+		this.$http=$http
+	}
+	
+	getPizzas(){
+		return this.$http.get('http.localhost:8000/pizzas').then(r=>r.data)
+	}	
+} 
