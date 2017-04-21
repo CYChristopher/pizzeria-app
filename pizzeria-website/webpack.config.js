17daacad103e0ed478a8a29fa6095a01c94fe4b9
Paola Lambroni <paolalambroni@gmail.com>
2017-04-21 14:25:25 +0200

    #16 usw001 accueil

--- pizzeria-website/webpack.config.js
+++ pizzeria-website/webpack.config.js
@@ -1,24 +1,23 @@
  module.exports = {
      entry: './src/app.js',
      output: {
-        path: './public',
-        filename: 'app.bundle.js'
-    },
+         path: './public',
+         filename: 'app.bundle.js'
+     },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.html$/,
-                loader: 'raw-loader'
-            }
-        ]
-    },
+     module: {
+         loaders: [{
+                 test: /\.js$/,
+                 exclude: /node_modules/,
+                 loader: 'babel-loader'
+             },
+             {
+                 test: /\.html$/,
+                 loader: 'raw-loader'
+             }
+         ]
+     },
 
-    devtool: 'source-map'
+     devtool: 'source-map'
 
  };
