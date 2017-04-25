b0df8672ad3bded08a6bf65d92bc591ca2b8381c
Strauss72 <loicguen@gmail.com>
2017-04-25 17:33:57 +0200

    #22 usw007 creation repo et ressource

---
+++ pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/repository/ICommandeRepository.java
@@ -0,0 +1,11 @@
+package fr.pizzeria.spring.web.repository;
+
+
+import fr.pizzeria.model.Commande;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ICommandeRepository extends JpaRepository<Commande, Integer> {
+
+	Commande findById(String id);
+}
---
+++ pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/resource/CommandeResource.java
@@ -0,0 +1,29 @@
+package fr.pizzeria.spring.web.resource;
+
+import fr.pizzeria.model.Commande;
+import fr.pizzeria.spring.web.repository.ICommandeRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Resource Commande.
+ */
+@RestController
+@RequestMapping("/commandes")
+public class CommandeResource {
+
+	@Autowired private ICommandeRepository commandeDao;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Commande> listAllCommandes() {
+		return this.commandeDao.findAll();
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public void addCommande(Commande commande) {
+		this.commandeDao.save(commande);
+	}
+}
