acc70956b676713ad4714253746fbb461dd9d2bc
Strauss72 <loicguen@gmail.com>
2017-04-25 16:31:24 +0200

    #17 usw002 des promesses

--- pizzeria-website/src/shared/service/pizza.service.js
+++ pizzeria-website/src/shared/service/pizza.service.js
@@ -1,14 +1,15 @@
 export class PizzaService{
 	
-	constructor($http, localStorageService){
+	constructor($http, localStorageService, $q){
 		this.localStorageService = localStorageService
 		this.$http=$http
+		this.$q=$q
 	}
 	
 	getPizzas(){
 		if(!this.localStorageService.get('pizzas')){
 			this.$http.get('http://localhost:8080/pizzas').then(r=>this.localStorageService.set('pizzas',r.data))
 		}
-		return this.localStorageService.get('pizzas')
+		return this.$q.resolve(this.localStorageService.get('pizzas'))
 	}	
 } 
