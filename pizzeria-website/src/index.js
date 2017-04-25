7bfc23a9cadadc58ffe0d9a4c9e33611695e914d
Christopher CHARLERY <charlery.christopher@gmail.com>
2017-04-25 09:53:15 +0200

    Fonctionnalité s'inscrire terminée

--- pizzeria-website/src/index.js
+++ pizzeria-website/src/index.js
@@ -4,13 +4,15 @@ import { HomeComponent } from './home.component'
 
 import {routes} from './routes'
 import {PizzaService} from './shared/service/pizza.service'
+import {ClientService} from './shared/service/client.service'
 import {PizzaComponent} from './pizza.component'
 import {ListePizzasComponent} from './listePizzas.component'
 import {AjouterPanierComponent} from './ajouterPanier.component'
-
+import { InscriptionComponent } from './inscription/index'
 
 
 angular.module('pizzeria', [ngRoute, 'LocalStorageModule'])
+.value('API_URL', 'http://localhost:8080')
 	.config(routes)
     .config(function ($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
@@ -21,9 +23,11 @@ angular.module('pizzeria', [ngRoute, 'LocalStorageModule'])
     }])
 
 .service('PizzaService',PizzaService)
+.service('ClientService', ClientService)
 .component('pizza',PizzaComponent)
 .component('listePizzas',ListePizzasComponent)
 .component('home', HomeComponent)
 .component('ajouterPanier', AjouterPanierComponent)
+.component('inscriptionComponent', InscriptionComponent)
 
 
