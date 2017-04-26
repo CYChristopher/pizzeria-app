1ccb8f916e1d1a6d7d70541af684973447a9129e
Christopher CHARLERY <charlery.christopher@gmail.com>
2017-04-26 10:07:41 +0200

    #20 USWOO5-S'inscrire

--- pizzeria-website/src/inscription/index.js
+++ pizzeria-website/src/inscription/index.js
@@ -1,5 +1,3 @@
-<<<<<<< Upstream, based on branch 'develop' of git@github.com:DTAFormation/pizzeria-app.git
-<<<<<<< Upstream, based on branch 'develop' of git@github.com:DTAFormation/pizzeria-app.git
 import template from './inscription.html';
 
 class controller {
@@ -30,61 +28,4 @@ class controller {
 export const InscriptionComponent = {
     controller,
     template,
-=======
-import template from './inscription.html';
-
-class controller {
-
-    constructor(UserService) {
-        this.UserService = UserService;
-        this.user = {
-            email : '',
-            mdp : '',
-            nom : '',
-            prenom : '',
-        }
-        this.confMdp = '';
-    }
-
-    validerForm(){
-        console.log(this.UserService);
-    }
-}
-
-export const InscriptionComponent = {
-    controller,
-    template,
->>>>>>> d09eecd Création de l'ihm d'inscription
-=======
-import template from './inscription.html';
-
-class controller {
-
-    constructor(ClientService, $location) {
-        this.$location = $location;
-        this.ClientService = ClientService;
-        this.client = {
-            email : '',
-            motDePasse : '',
-            nom : '',
-            prenom : '',
-        }
-        this.confMdp = '';
-    }
-
-    validerForm(userForm){
-        if(userForm.$valid){
-            this.ClientService.saveClient(this.client)
-                .then(response => {
-                    //Modifier la redirection
-                    this.$location.path('/');
-                });
-        }
-    }
-}
-
-export const InscriptionComponent = {
-    controller,
-    template,
->>>>>>> 9448abd Fonctionnalité s'inscrire terminée
 }
