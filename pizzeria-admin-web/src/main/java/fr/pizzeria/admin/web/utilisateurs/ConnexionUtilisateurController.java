20f4f4c05df748061dde27ef5c9ff3fc0ac4bcc1
Valentin Giboulot <valentin.giboulot@gmail.com>
2017-04-28 12:34:50 +0200

    correction du hash pour réparer l'authentification en prod

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/utilisateurs/ConnexionUtilisateurController.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/utilisateurs/ConnexionUtilisateurController.java
@@ -40,11 +40,10 @@ public class ConnexionUtilisateurController extends HttpServlet {
 		LOG.log(Level.INFO, "utilisateur != null :" + (utilisateur != null));
 		LOG.log(Level.INFO, "utilisateur.motDePasseOk(motDePasse) :" + utilisateur.motDePasseOk(motDePasse));
 		LOG.log(Level.INFO, "motDePasse :" + motDePasse);
-		LOG.log(Level.INFO,
-				"motDePasse hashé:" + utilisateur.hashSha1(motDePasse, utilisateur.getDateCreation().toString()));
+		LOG.log(Level.INFO, "motDePasse hashé:" + utilisateur.hashSha1(motDePasse, utilisateur.getDateCreation()));
 		LOG.log(Level.INFO, "hash en base:" + utilisateur.getMotDePasse());
 
-		LOG.log(Level.INFO, "date récupéré en base avec un toString()" + utilisateur.getDateCreation().toString());
+		LOG.log(Level.INFO, "date récupéré en base avec un toString()" + utilisateur.getDateCreation());
 
 		if (utilisateur != null && utilisateur.motDePasseOk(motDePasse)) {
 			HttpSession session = request.getSession();
