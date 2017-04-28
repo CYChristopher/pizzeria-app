23750135b5bcca3eaef365ad2b8896a740e6423f
Lionel <lionel.collidor2016@campus-eni.fr>
2017-04-28 14:18:11 +0200

    usa015-offres-speciales-menu #15
    modification de la servlet ListerOffresMenuController:
    - modification doGet pour prise en compte des modification du service
    (exception)
    - doPost (archivage du menu)

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/offreMenu/ListerOffreMenuController.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/offreMenu/ListerOffreMenuController.java
@@ -1,8 +1,11 @@
 package fr.pizzeria.admin.web.offreMenu;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.persistence.NoResultException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,19 +15,44 @@ import javax.servlet.http.HttpServletResponse;
 
 import fr.pizzeria.admin.metier.OffreMenuService;
 
-@WebServlet("/offre_menu/liste")
+@WebServlet("/offre_menus/liste")
 public class ListerOffreMenuController extends HttpServlet {
+	
+	private static final Logger LOG = Logger.getLogger(ListerOffreMenuController.class.getName());
 
-	private static final String VUE_LISTER_OFFRE_MENU = "/WEB-INF/views/offreMenu/listerOffreMenu.jsp";
+	private static final String VUE_LISTER_OFFRE_MENU = "/WEB-INF/views/offreMenus/listerOffreMenu.jsp";
 
 	@Inject
 	private OffreMenuService oms;
-
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setAttribute("listeOffreMenu", this.oms.findAll());
-		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(VUE_LISTER_OFFRE_MENU);
-		dispatcher.forward(req, resp);
+		try {
+			req.setAttribute("listeOffreMenu", this.oms.findAll());
+		} catch (NoResultException e) {
+			LOG.log(Level.WARNING, "-------!!!------- exception levée : " + e.getMessage() + " => " + e.getCause());
+			req.setAttribute("msg", "Erreur du serveur, merci de contacter le support de l'application ");
+		} finally {
+			RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(VUE_LISTER_OFFRE_MENU);
+			dispatcher.forward(req, resp);
+		}
 	}
-
+	
+	/**
+	 * Méthode de d'archivage de l'offre menu
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer id = Integer.valueOf(req.getParameter("id").toString());
+		LOG.log(Level.INFO, "-------!!!------- suppression/archivage de l'ingredient n : " + id);
+		
+		try {
+			oms.archive(id);
+		} catch (NoResultException e) {
+			LOG.log(Level.WARNING, "-------!!!------- exception levée : " + e.getMessage() + " => " + e.getCause());
+			req.setAttribute("msg", "Erreur du serveur, merci de contacter le support de l'application ");
+		} finally {
+			resp.sendRedirect(req.getContextPath() + "/offre_menus/liste");
+		}
+	}
 }
