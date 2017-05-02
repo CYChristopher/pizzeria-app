0e61f2189f9d8033bd2d32a85a28072cfd71e406
David Le Hardy <lehardy.david@live.fr>
2017-05-02 15:28:05 +0200

    #81 clean recuperation des pizzas visible par l'utilisateur, ainsi que les Ouiches

--- pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/repository/IPizzaRepository.java
+++ pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/repository/IPizzaRepository.java
@@ -16,13 +16,11 @@ public interface IPizzaRepository extends JpaRepository<Pizza,Integer> {
 
     Pizza findByCode(String code);
     
-    @Modifying
-    @Transactional
+   
     @Query("select pizza from Pizza pizza where pizza.typePizza='PIZZA' and pizza.actif=true")
     List<Pizza> findPizzaActual();
     
-    @Modifying
-    @Transactional
+    
     @Query("select pizza from Pizza pizza where pizza.typePizza='OUICHE' and pizza.actif=true")
     List<Pizza> findOuicheActual();
     
