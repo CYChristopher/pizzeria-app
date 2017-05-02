c7c426d4c8de9a8813746a3f6153f6edd71ddc78
Tanguy Gigarel <gigarelt@gmail.com>
2017-05-02 15:50:15 +0200

    correction majeure

--- pizzeria-website/src/index.js
+++ pizzeria-website/src/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
 
+/* beautify preserve:start */
 import { routes } from './routes'
 import { PizzaService } from './shared/service/pizza.service'
 import { ClientService } from './shared/service/client.service'
@@ -13,26 +14,29 @@ import { AjouterPanierComponent } from './ajouterPanier.component'
 import { PanierComponent } from './panier.component'
 import { NavbarComponent } from './navbar.component'
 import { MonCompteComponent } from './monCompte.component'
+import { PanierIndicateurComponent } from './panierIndicateur.component'
+/* beautify preserve:end */
 
-
-angular.module('pizzeria', [ngRoute, 'LocalStorageModule'])
-    .value('API_URL', BACKEND_API_URL)
-    .config(routes)
-    .config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-    })
-    .config(['localStorageServiceProvider', function(localStorageServiceProvider) {
-        localStorageServiceProvider
-            .setPrefix('pizzeriaLS')
-    }])
-    .service('PizzaService', PizzaService)
-    .service('ClientService', ClientService)
-    .component('pizza', PizzaComponent)
-    .component('listePizzas', ListePizzasComponent)
-    .component('home', HomeComponent)
-    .component('ajouterPanier', AjouterPanierComponent)
-    .component('inscriptionComponent', InscriptionComponent)
-    .component('connexion', ConnexionComponent)
-    .component('panier', PanierComponent)
-    .component('navbar', NavbarComponent)
-    .component('monCompte', MonCompteComponent)
+angular.module( 'pizzeria', [ ngRoute, 'LocalStorageModule' ] )
+  .value( 'API_URL', BACKEND_API_URL )
+  .config( routes )
+  .config( function ( $routeProvider, $locationProvider ) {
+    $locationProvider.html5Mode( true );
+  } )
+  .config( [ 'localStorageServiceProvider', function (
+    localStorageServiceProvider ) {
+    localStorageServiceProvider
+      .setPrefix( 'pizzeriaLS' )
+    } ] )
+  .service( 'PizzaService', PizzaService )
+  .service( 'ClientService', ClientService )
+  .component( 'pizza', PizzaComponent )
+  .component( 'listePizzas', ListePizzasComponent )
+  .component( 'home', HomeComponent )
+  .component( 'ajouterPanier', AjouterPanierComponent )
+  .component( 'inscriptionComponent', InscriptionComponent )
+  .component( 'connexion', ConnexionComponent )
+  .component( 'panier', PanierComponent )
+  .component( 'navbar', NavbarComponent )
+  .component( 'monCompte', MonCompteComponent )
+  .component( 'panierIndicateur', PanierIndicateurComponent )
