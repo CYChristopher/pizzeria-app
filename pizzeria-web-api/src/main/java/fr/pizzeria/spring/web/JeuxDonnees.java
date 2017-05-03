f3c05a7007b0e09a75b010bb9275c05299eaae98
Lionel <lionel.collidor2016@campus-eni.fr>
2017-05-03 10:01:53 +0200

    usw024-jeux-donnees-83 #83
    modification des attributs des pizzas.
    ajout de commande.

--- pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/JeuxDonnees.java
+++ pizzeria-web-api/src/main/java/fr/pizzeria/spring/web/JeuxDonnees.java
@@ -9,9 +9,11 @@ import java.util.List;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import fr.pizzeria.model.Boisson;
 import fr.pizzeria.model.CategoriePizza;
 import fr.pizzeria.model.Client;
 import fr.pizzeria.model.Commande;
+import fr.pizzeria.model.Dessert;
 import fr.pizzeria.model.Ingredient;
 import fr.pizzeria.model.Livreur;
 import fr.pizzeria.model.Pizza;
@@ -33,6 +35,8 @@ public class JeuxDonnees {
 	private List<Livreur> livreurs = new ArrayList<>();
 	private List<Commande> commandes = new ArrayList<>();
 	private List<Ingredient> ingredients = new ArrayList<>();
+	private List<Boisson> boissons = new ArrayList<>();
+	private List<Dessert> desserts = new ArrayList<>();
 	
 	public JeuxDonnees(){
 		generateClients();
@@ -50,6 +54,10 @@ public class JeuxDonnees {
 		clients.add(new Client("Lille", "Jon", "l.j@gmail.com", DigestUtils.sha256Hex("password954"), "Au nord"));
 	}
 	
+	private void generateBoissons(){
+		
+	}
+	
 	private void generateIngredients(){
 		ingredients.add(new Ingredient("Totomate", 100, 1.5));
 		ingredients.add(new Ingredient("Maurizzella", 50, 3.5));
@@ -62,21 +70,21 @@ public class JeuxDonnees {
 		pizzas.add(new Pizza("PEP", "Peperoni", BigDecimal.valueOf(12.50), CategoriePizza.VIANDE,
 				"https://www.tutti-pizza.com/images/350x270/produit/_/marguerita-jambon_marguerita-jambon.jpg", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("4FRO", "4 Fromages", BigDecimal.valueOf(14.70), CategoriePizza.SANS_VIANDE,
-				"http://www.cicis.com/media/1177/pizza_trad_cheese_sm.png", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
+				"http://www.cicis.com/media/1177/pizza_trad_cheese_sm.png", LocalDateTime.now(), true, TypePizza.OUICHE,  ingredients));
 		pizzas.add(new Pizza("VEG", "Végétarienne", BigDecimal.valueOf(11.30), CategoriePizza.SANS_VIANDE,
 				"http://www.cicis.com/media/1173/pizza_trad_veggie_sm.png", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("SAUC", "Saucisses", BigDecimal.valueOf(10.50), CategoriePizza.VIANDE,
-				"http://www.cicis.com/media/1169/pizza_trad_sausage_sm.png", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
+				"http://www.cicis.com/media/1169/pizza_trad_sausage_sm.png", LocalDateTime.now(), true, TypePizza.OUICHE,  ingredients));
 		pizzas.add(new Pizza("SAUM", "Saumon", BigDecimal.valueOf(10.50), CategoriePizza.POISSON,
-				"https://www.dominos.fr/ManagedAssets/FR/product/PSSE/FR_PSSE_fr_hero_1146.png?v1112248177", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
+				"https://www.dominos.fr/ManagedAssets/FR/product/PSSE/FR_PSSE_fr_hero_1146.png?v1112248177", LocalDateTime.now(), false, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("BEF", "Boeuf", BigDecimal.valueOf(13.20), CategoriePizza.VIANDE,
 				"http://www.cicis.com/media/1170/pizza_trad_beef_sm.png", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("POUL", "Poulet", BigDecimal.valueOf(13.20), CategoriePizza.VIANDE,
-				"http://www.cicis.com/media/1336/buffalo-chicken_new72dpi.png", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
+				"http://www.cicis.com/media/1336/buffalo-chicken_new72dpi.png", LocalDateTime.now(), false, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("KEB", "Kebab", BigDecimal.valueOf(16.80), CategoriePizza.VIANDE,
 				"https://www.dominos.fr/ManagedAssets/FR/product/PSKE/FR_PSKE_fr_hero_997.png?v-1581416390", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("REI", "Reine", BigDecimal.valueOf(16.80), CategoriePizza.VIANDE,
-				"https://www.dominos.fr/ManagedAssets/FR/product/PREI/FR_PREI_fr_hero_541.png?v-1935795394", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
+				"https://www.dominos.fr/ManagedAssets/FR/product/PREI/FR_PREI_fr_hero_541.png?v-1935795394", LocalDateTime.now(), false, TypePizza.PIZZA,  ingredients));
 		pizzas.add(new Pizza("ORI", "Orientale", BigDecimal.valueOf(16.80), CategoriePizza.VIANDE,
 				"https://www.dominos.fr/ManagedAssets/FR/product/PORI/FR_PORI_fr_hero_541.png?v334099666", LocalDateTime.now(), true, TypePizza.PIZZA,  ingredients));
 	}
@@ -103,5 +111,16 @@ public class JeuxDonnees {
 		c1.setDateCommande(LocalDateTime.of(2016, Month.AUGUST, 24, 18, 38));
 		c1.setLivreur(livreurs.get(0));
 		
+		Commande c2 = new Commande();
+		c2.setNumeroCommande("KLP456");
+		c2.setStatut(StatutCommande.EN_PREPARATION);
+		c2.setDateCommande(LocalDateTime.of(2016, Month.AUGUST, 24, 18, 38));
+		c2.setLivreur(livreurs.get(1));
+		
+		Commande c3 = new Commande();
+		c3.setNumeroCommande("JTS469");
+		c3.setStatut(StatutCommande.NON_TRAITE);
+		c3.setDateCommande(LocalDateTime.now());
+		c3.setLivreur(livreurs.get(3));
 	}
 }
