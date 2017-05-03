3cc51efdd779595acf9ad297de796ae1cd3e97e8
Nicolas Amiot <amiot-nicolas@orange.fr>
2017-05-03 10:17:51 +0200

    #18 Review ok

--- pizzeria-website/src/ajouterPanier.component/index.js
+++ pizzeria-website/src/ajouterPanier.component/index.js
@@ -1,30 +1,32 @@
 import template from './ajouterPanier.html';
 
 class controller {
-    constructor(localStorageService) {
-        this.StockageService = localStorageService;
+  constructor( localStorageService ) {
+    this.StockageService = localStorageService;
+  }
+  ajouterAuStockageLocal() {
+    let contenuStockage = this.StockageService.get( 'panier', 'localStorage' );
+    if ( contenuStockage === null ) {
+      contenuStockage = [];
     }
-    ajouterAuStockageLocal() {
-        let contenuStockage = this.StockageService.get('panier','localStorage');
-        if (contenuStockage === null) {
-            contenuStockage = [];
-        }
-        
-        let index = contenuStockage.findIndex(panierItem => this.item.id === parseInt(panierItem.id));
-        if (index >= 0){
-            contenuStockage[index].quantite++;
-        }
-        else {
-            contenuStockage.push({ id: `${this.item.id}`, quantite: 1 });
-        }
-        this.StockageService.set('panier', contenuStockage, 'localStorage');
+    let index = contenuStockage.findIndex( panierItem => this.item.id ===
+      parseInt( panierItem.id ) );
+    if ( index >= 0 ) {
+      contenuStockage[ index ].quantite++;
+    } else {
+      contenuStockage.push( {
+        id: `${this.item.id}`,
+        quantite: 1
+      } );
     }
+    this.StockageService.set( 'panier', contenuStockage, 'localStorage' );
+  }
 }
 
 export const AjouterPanierComponent = {
-    bindings: {
-        item: '<'
-    },
-    controller,
-    template
+  bindings: {
+    item: '<'
+  },
+  controller,
+  template
 }
